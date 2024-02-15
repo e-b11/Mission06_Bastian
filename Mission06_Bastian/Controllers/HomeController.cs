6bfa58c5f0f4cc1b7be88f4d38f1f2a35cbb4f13
e-b11 <96320276+e-b11@users.noreply.github.com>
2024-02-15 13:35:37 -0700

    basics

--- Mission06_Bastian/Controllers/HomeController.cs
+++ Mission06_Bastian/Controllers/HomeController.cs
@@ -5,28 +5,21 @@ using System.Diagnostics;
 namespace Mission06_Bastian.Controllers
 {
     public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+    {      
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult GetToKnowJoel()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult MovieForm()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
+      
     }
 }
---
+++ Mission06_Bastian/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mission06_Bastian.Models
+{
+    public class Movie
+    {
+        [Key]
+        [Required]
+        public int MovieID { get; set; }
+        public string Category { get; set; }
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string Director { get; set; }
+        public string Rating { get; set; }
+        public bool Edited { get; set; }
+        public string LentTo { get; set; }
+        public string Notes { get; set; }
+
+    }
+}
