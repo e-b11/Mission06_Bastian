b3cb1aae79dac42438833b412eb336921cfdff38
e-b11 <96320276+e-b11@users.noreply.github.com>
2024-02-15 17:26:41 -0700

    database

--- Mission06_Bastian/Controllers/HomeController.cs
+++ Mission06_Bastian/Controllers/HomeController.cs
@@ -5,7 +5,12 @@ using System.Diagnostics;
 namespace Mission06_Bastian.Controllers
 {
     public class HomeController : Controller
-    {      
+    {
+        private MovieContext _context;
+        public HomeController(MovieContext temp) //Constructor
+        {
+            _context = temp;
+        }
         public IActionResult Index()
         {
             return View();
@@ -16,10 +21,22 @@ namespace Mission06_Bastian.Controllers
             return View();
         }
 
+        [HttpGet]
         public IActionResult MovieForm()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult MovieForm(Movie response)
+        {
+            _context.Movies.Add(response);
+            _context.SaveChanges();
+
+            return View("Confirmation", response);
+        }
+        
+        
       
     }
 }
---
+++ Mission06_Bastian/Models/MovieContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Mission06_Bastian.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options) : base(options) //Constructor
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
